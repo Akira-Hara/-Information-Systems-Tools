e505db75374527e30951e52c3e98c4d9b2ad5d06
Алена <153750192+Akira-Hara@users.noreply.github.com>
2024-01-08 16:42:47 +0700

    Add a new folder and a new file

---
+++ lad8/html/search/all_0.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['main_0',['main',['../namespacemanage.html#a0ac64ea81648f42d6a49d76da894eaf4',1,'manage']]],
+  ['manage_1',['manage',['../namespacemanage.html',1,'']]],
+  ['mysite_3a_3aasgi_2',['asgi',['../namespacemysite_1_1asgi.html',1,'mysite']]],
+  ['mysite_3a_3asettings_3',['settings',['../namespacemysite_1_1settings.html',1,'mysite']]],
+  ['mysite_3a_3aurls_4',['urls',['../namespacemysite_1_1urls.html',1,'mysite']]],
+  ['mysite_3a_3awsgi_5',['wsgi',['../namespacemysite_1_1wsgi.html',1,'mysite']]]
+];
---
+++ lad8/html/search/namespaces_0.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['manage_0',['manage',['../namespacemanage.html',1,'']]],
+  ['mysite_3a_3aasgi_1',['asgi',['../namespacemysite_1_1asgi.html',1,'mysite']]],
+  ['mysite_3a_3asettings_2',['settings',['../namespacemysite_1_1settings.html',1,'mysite']]],
+  ['mysite_3a_3aurls_3',['urls',['../namespacemysite_1_1urls.html',1,'mysite']]],
+  ['mysite_3a_3awsgi_4',['wsgi',['../namespacemysite_1_1wsgi.html',1,'mysite']]]
+];
